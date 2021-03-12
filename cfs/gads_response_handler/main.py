5444de57b025b9288bf329dbdb7037233706764e
Jaimemm <jaimemm@google.com>
2021-03-12 12:09:10 +0100

    Skeleton upload
    Change-Id: I9942a5d83282a3fc2252a48f5079f0df1320b4cd

---
+++ cfs/gads_response_handler/main.py
@@ -0,0 +1,52 @@
+"""Google Cloud function that sends batches of clients to predict."""
+
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# -*- coding: utf-8 -*-
+
+import base64
+import datetime
+import json
+import os
+import re
+import sys
+import uuid
+# import pandas as pd
+# import numpy as np
+
+from typing import Any, Dict, Optional
+from google.cloud.functions_v1.context import Context
+from google.cloud import pubsub_v1
+from google.cloud import firestore
+
+import pytz
+
+
+def main(event: Dict[str, Any],
+         context=Optional[Context]):
+  """Triggers the message processing.
+
+  Args:
+    event (dict):  The dictionary with data specific to this type of event. The
+      `data` field contains the PubsubMessage message. The `attributes` field
+      will contain custom attributes if there are any.
+    context (google.cloud.functions.Context): The Cloud Functions event
+      metadata. The `event_id` field contains the Pub/Sub message ID. The
+      `timestamp` field contains the publish time.
+  """
+  del context
+  print("Main")
+  
+
